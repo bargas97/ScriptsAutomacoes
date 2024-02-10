3c9931ab54dec50d03f602ddb6875e8aafe86769
gabriel.bargas <gabriel.bargas@totvs.com.br>
2024-02-10 19:26:46 -0300

    teste handler

--- TesteHello.py
+++ TesteHello.py
@@ -1,1 +1,8 @@
-print("deu bom")
+from datetime import datetime
+
+def handler(event, context):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return {
+        'statusCode': 200,
+        'body': f'A hora atual é: {current_time}'
+    }
